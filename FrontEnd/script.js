6c82f55a8fb402d13f7f5791542d1a9336aa2244
lebrun26 <lebrun.patrickdev@gmail.com>
2024-04-08 14:35:41 +0200

    Finalisation des filtres et abandon de la methode du commit precedent

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,7 +5,7 @@ console.log(projet)
 // Appel et mise en place des projets pour la page d'accueil
 const gallery = document.querySelector(".gallery")
 
-function genereProjet(){
+function genereProjet(projet){
     for(let i = 0; i < projet.length; i++){
         const projetAccueil = projet[i]
         const figure = document.createElement("figure")
@@ -21,5 +21,80 @@ function genereProjet(){
 }
 genereProjet(projet)
 
+
 // Filtre de la page
 
+//boutton "Tous"
+const filter = document.createElement("div")
+filter.classList = "container__filter"
+const btnTous = document.createElement("button")
+btnTous.classList = "btn__filter"
+btnTous.textContent = "Tous"
+filter.appendChild(btnTous)
+btnTous.addEventListener("click", () =>{
+    console.log("Vous avez cliqué sur le bouton Tous")
+    // Faire verifier variable a pierre car elle n'est pas lu
+    const tousfilter = projet.filter( (tous) =>{
+        return projet
+    
+    })
+    document.querySelector(".gallery").innerHTML = ""
+    genereProjet(projet)
+    console.log(projet)
+})
+
+//Boutton "Objets"
+const btnObjets = document.createElement("button")
+btnObjets.classList = "btn__filter"
+btnObjets.textContent = "Objets"
+filter.appendChild(btnObjets)
+btnObjets.addEventListener("click", () =>{
+    console.log("Vous avez cliqué sur le bouton Objets")
+    const objetsfilter = projet.filter( (objets) =>{
+        return objets.categoryId === 1
+    
+    })
+    document.querySelector(".gallery").innerHTML = ""
+    genereProjet(objetsfilter)
+    console.log(objetsfilter)
+})
+
+
+// Boutton "Appartements"
+const btnAppartements = document.createElement("button")
+btnAppartements.classList = "btn__filter"
+btnAppartements.textContent = "Appartements"
+filter.appendChild(btnAppartements)
+btnAppartements.addEventListener("click", () =>{
+    console.log("Vous avez cliqué sur le bouton Appartement")
+    const appartementsfilter = projet.filter( (appartements) =>{
+        return appartements.categoryId === 2
+    
+    })
+    document.querySelector(".gallery").innerHTML = ""
+    genereProjet(appartementsfilter)
+    console.log(appartementsfilter)
+})
+
+// Boutton "Hotel"
+const btnHotel = document.createElement("button")
+btnHotel.classList = "btn__filter"
+btnHotel.textContent = "Hotel & restaurants"
+filter.appendChild(btnHotel)
+btnHotel.addEventListener("click", () =>{
+    console.log("Vous avez cliqué sur le bouton Hotel")
+    const hotelfilter = projet.filter( (hotel) =>{
+        return hotel.categoryId === 3
+    
+    })
+    document.querySelector(".gallery").innerHTML = ""
+    genereProjet(hotelfilter)
+    console.log(hotelfilter)
+})
+
+// Rattachement à son parent portfolio
+const portfolio = document.querySelector("#portfolio")
+portfolio.appendChild(filter)
+
+// je met la div crée filter avant la div gallery
+portfolio.insertBefore(filter, gallery)
