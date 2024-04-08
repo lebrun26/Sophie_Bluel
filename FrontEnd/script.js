b773d4e6e1680d986fdbd94021c6e371babf4faf
lebrun26 <lebrun.patrickdev@gmail.com>
2024-04-08 09:32:08 +0200

    finalisation de l'appel de la gallery + title dans la page d'accueil

---
+++ FrontEnd/script.js
@@ -0,0 +1,25 @@
+const reponse = await fetch("http://localhost:5678/api/works")
+const projet = await reponse.json()
+console.log(projet)
+
+// Appel et mise en place des projets pour la page d'accueil
+const gallery = document.querySelector(".gallery")
+
+function genereProjet(){
+    for(let i = 0; i < projet.length; i++){
+        console.log("Projet en cours :", projet[i])
+        const projetAccueil = projet[i]
+        const figure = document.createElement("figure")
+        const img = document.createElement("img")
+        img.src = projetAccueil.imageUrl
+        figure.appendChild(img)
+        const figcaption = document.createElement("figcaption")
+        figcaption.textContent = projetAccueil.title
+        figure.appendChild(figcaption)
+
+        gallery.appendChild(figure)
+    } 
+}
+genereProjet(projet)
+
+
